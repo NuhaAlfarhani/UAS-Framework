616cab4d275fdfa9bb12c4548f9309e6d4880a1f
fidiramadhina <9hwahyufidi23@gmail.com>
2023-05-31 10:21:06 +0700

    update last aman

--- routes/web.php
+++ routes/web.php
@@ -23,37 +23,40 @@ Route::group(['middleware' => 'guest'], function () {
     Route::post('/login', [LoginController::class, 'loginPost'])->name('login');
 });
 
-//Route untuk Home
-Route::get('/home', function() {return view('view_home');});
+Route::group(['middleware'=>'auth'], function(){
+    //Route untuk Home
+    Route::get('/home', function () {
+        return view('view_home');
+    });
 
-//Route untuk Data Buku
-Route::get('/buku', 'BukuController@bukutampil');
-Route::post('/buku/tambah','BukuController@bukutambah');
-Route::get('/buku/hapus/{id_buku}','BukuController@bukuhapus');
-Route::put('/buku/edit/{id_buku}', 'BukuController@bukuedit');
+    //Route untuk Data Buku
+    Route::get('/buku', 'BukuController@bukutampil');
+    Route::post('/buku/tambah', 'BukuController@bukutambah');
+    Route::get('/buku/hapus/{id_buku}', 'BukuController@bukuhapus');
+    Route::put('/buku/edit/{id_buku}', 'BukuController@bukuedit');
 
-//Route untuk Data Buku
-Route::get('/home', function(){return view('view_home');});
+    //Route untuk Data Buku
+    Route::get('/home', function () {
+        return view('view_home');
+    });
 
-//Route untuk Data Anggota
-Route::get('/anggota', 'AnggotaController@anggotatampil');
-Route::post('/anggota/tambah', 'AnggotaController@anggotatambah');
-Route::get('/anggota/hapus/{id_anggota}', 'AnggotaController@anggotahapus');
-Route::put('/anggota/edit/{id_anggota}', 'AnggotaController@anggotaedit');
+    //Route untuk Data Anggota
+    Route::get('/anggota', 'AnggotaController@anggotatampil');
+    Route::post('/anggota/tambah', 'AnggotaController@anggotatambah');
+    Route::get('/anggota/hapus/{id_anggota}', 'AnggotaController@anggotahapus');
+    Route::put('/anggota/edit/{id_anggota}', 'AnggotaController@anggotaedit');
 
-//Route untuk Data Petugas
-Route::get('/petugas', 'PetugasController@petugastampil');
-Route::post('/petugas/tambah', 'PetugasController@petugastambah');
-Route::get('/petugas/hapus/{id_petugas}', 'PetugasController@petugashapus');
-Route::put('/petugas/edit/{id_petugas}', 'PetugasController@petugasedit');
+    //Route untuk Data Petugas
+    Route::get('/petugas', 'PetugasController@petugastampil');
+    Route::post('/petugas/tambah', 'PetugasController@petugastambah');
+    Route::get('/petugas/hapus/{id_petugas}', 'PetugasController@petugashapus');
+    Route::put('/petugas/edit/{id_petugas}', 'PetugasController@petugasedit');
 
-//Route untuk Data Peminjaman
-Route::get('/pinjam', 'PinjamController@pinjamtampil');
-Route::post('/pinjam/tambah','PinjamController@pinjamtambah');
-Route::get('/pinjam/hapus/{id_pinjam}','PinjamController@pinjamhapus');
-Route::put('/pinjam/edit/{id_pinjam}', 'PinjamController@pinjamedit');
+    //Route untuk Data Peminjaman
+    Route::get('/pinjam', 'PinjamController@pinjamtampil');
+    Route::post('/pinjam/tambah', 'PinjamController@pinjamtambah');
+    Route::get('/pinjam/hapus/{id_pinjam}', 'PinjamController@pinjamhapus');
+    Route::put('/pinjam/edit/{id_pinjam}', 'PinjamController@pinjamedit');
 
-//Route untuk Logout
-Route::group(['middleware' => 'auth'], function () {
     Route::delete('/logout', [LoginController::class, 'logout'])->name('logout');
 });
